a1e34f58dd12484a3b167dafc1eca674108eaf63
Megalapa <megalapa@MBA.local>
2017-06-09 11:03:46 +0300

    add Controller

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -1,2 +1,43 @@
 class FacilitiesController < ApplicationController
+  before_action :facility_load, only: [:show, :edit, :update, :delete]
+
+  respond_to :html #, :json
+  
+  def index
+    respond_with(@facilities = Facility.all)
+  end
+
+  def new
+    @facility = Facility.new(facilities_params)
+    respond_with @facility
+  end
+
+  def create	
+    @facility = Facility.new(facilities_params)
+    respond_with @facility
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    respond_with(@facility.update(facilities_params))
+  end
+
+  def destroy
+    @facility.delete
+  end
+
+  private
+
+    def facility_load
+      @facility = Facility.find params[:id]
+    end
+
+    def facilities_params
+      require(:facility).permit(:name, :supervisor, :address, :manager)
+    end
 end
--- spec/models/facility_spec.rb
+++ spec/models/facility_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe Facility, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:address) }
