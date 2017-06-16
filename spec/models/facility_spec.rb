a9f641000e216f47bfd6257aca67ca518213849c
Megalapa <megalapa@MBA.local>
2017-06-16 17:32:01 +0300

    add fewer tests

--- spec/models/facility_spec.rb
+++ spec/models/facility_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe Facility, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:address) }
+  [:name, :supervisor, :address, :manager].each do |collumn|
+    it { should validate_presence_of(collumn) }
+  # it { should validate_presence_of(:address) }
+  end
 end
