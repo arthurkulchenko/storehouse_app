2b795d2a9b09e3f12401dc9e1665b2abdb5b0a3c
Megalapa <megalapa@MBA.local>
2017-06-16 17:28:41 +0300

    seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # devise_for :users
-  root 'facility#index'
+  root 'facilities#index'
   resources :facilities
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do |f|
+  Facility.create!(
+    name: "name ##{f}",
+    supervisor: "supervisor ##{f}",
+    address: "address ##{f}",
+    manager: "manager ##{f}",
+  	)
+end
