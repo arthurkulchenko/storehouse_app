ddef00a1b6cfbd6eb2fb9b72da14f5e5bac80d32
Megalapa <megalapa@MBA.local>
2017-07-17 13:15:50 +0300

    fix mistake in assocition

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,4 +1,4 @@
 class Facility < ApplicationRecord
   validates :name, :supervisor, :address, :manager, presence: true
-  has_many :storages, dependent: :delete_all
+  has_one :storage, dependent: :delete
 end
--- spec/models/facility_spec.rb
+++ spec/models/facility_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Facility, type: :model do
   [:name, :supervisor, :address, :manager].each do |collumn|
     it { should validate_presence_of(collumn) }
-    it { should have_many(:storages).dependent(:delete_all) }
+    it { should have_one(:storage).dependent(:delete) }
   end
 end
