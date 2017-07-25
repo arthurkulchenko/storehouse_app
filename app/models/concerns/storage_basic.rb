251300e51b1f4bb54a06ea7ad2c647ca3dfc22b5
Megalapa <megalapa@MBA.local>
2017-07-25 17:26:12 +0300

    solve

--- app/models/concerns/storage_basic.rb
+++ app/models/concerns/storage_basic.rb
@@ -4,7 +4,7 @@ module StorageBasic
     belongs_to :facility
     validates :facility_id, presence: true
     [:consumables, :chem_consums, :equipment, :inventory].each do |model|
-      has_many model, as: :por, dependent: :destroy
+      has_many model, as: :por, class_name: "Storage::In::#{model.to_s.singularize.classify}", dependent: :destroy
       accepts_nested_attributes_for model, reject_if: :all_blank
     end
   end
